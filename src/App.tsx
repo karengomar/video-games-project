d294e5b6f199149a5440c9d2d5faac54a24ee2b4
karengomar <karen.gomar30@gmail.com>
2022-07-09 09:13:30 +0200

    modified nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import Board from './components/Board';
 import Detail from './components/Detail';
-import NavBar from './components/nav-bar.component';
-
 import {BrowserRouter as Router, Routes,Route} from "react-router-dom";
 
 const App =() => {
 return(
         <>
-        <NavBar />
         <Router>
             <Routes>
                 <Route path="/detail/:id" element={<Detail />} />
